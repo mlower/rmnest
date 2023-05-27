0bfb03d7e1716e9d26f4d8e4c6806053a5eed072
Pavan Uttarkar <pavan.uttarkar@gmail.com>
2023-05-27 21:25:23 +1000

    indentation f90

--- rmnest/circular.f90
+++ rmnest/circular.f90
@@ -19,7 +19,6 @@ subroutine circular(frequency,&
         integer(kind=4):: i,j
 
         !Defining constants
-        
         real(kind=8):: const_c
         real(kind=8):: const_mega
         real(kind=8):: deg2rad
@@ -34,11 +33,11 @@ subroutine circular(frequency,&
         real(kind=8), intent(in):: theta
         
         real(kind=8), intent(in), dimension(len_ch)       ::frequency
-        real(kind=8),  dimension(len_ch)       ::psi
+        real(kind=8),  dimension(len_ch)                  ::psi
 
-        real(kind=8),  dimension(len_ch, 3)       ::stokesp
-        real(kind=8),  dimension(3, 3)            ::rot_kernel
-        real(kind=8), intent(out), dimension(len_ch, 3)         ::rot_vector
+        real(kind=8),  dimension(len_ch, 3)               ::stokesp
+        real(kind=8),  dimension(3, 3)                    ::rot_kernel
+        real(kind=8), intent(out), dimension(len_ch, 3)   ::rot_vector
         
         const_c     =   299792458
         const_mega  =   1000000
@@ -52,15 +51,7 @@ subroutine circular(frequency,&
                 stokesp(i,2)   =   sin(2*psi(i))*cos(2*chi*deg2rad)
                 stokesp(i,3)   =   sin(2*chi*deg2rad) 
         end do
-        
-        !do i=1, 3 
-        !    do j=1, len_ch
-        !        sq(i)(j)   =   cos(2*psi(i))*cos(2*chi*deg2rad)
-        !        su(i)(j)   =   sin(2*psi(i))*cos(2*chi*deg2rad)
-        !        sv(i)(j)   =   sin(2*chi*deg2rad) 
-        !    end do
-        !end do
-        
+       
         !defining manualy :')
         rot_kernel(1,1)    =   cos(theta*deg2rad)*cos(phi*deg2rad)
         rot_kernel(1,2)    =  -cos(theta*deg2rad)*sin(phi*deg2rad)
@@ -71,11 +62,7 @@ subroutine circular(frequency,&
         rot_kernel(3,1)    =   -sin(theta*deg2rad)*cos(phi*deg2rad)
         rot_kernel(3,2)    =   sin(theta*deg2rad)*sin(phi*deg2rad)
         rot_kernel(3,3)    =   cos(theta*deg2rad)
-        
-        !mat_thea = array([[cos(theta)*cos(phi), -cos(theta)*sin(phi), sin(theta)],
-        ![sin(phi), cos(phi), 0], [-sin(theta)*cos(phi), sin(theta)*sin(phi),
-        !cos(theta)]]) 
-        
+       
         rot_vector          =   matmul(stokesp, rot_kernel)
 
         end subroutine circular
